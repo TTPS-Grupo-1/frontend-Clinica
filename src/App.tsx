af8ad8cb9ba5a89eea2a060ebd2f1c21ab342028
feliu <feliufacundo0@gmail.com>
2025-10-07 17:19:53 -0300

    agrego toast para mensajes

--- src/App.tsx
+++ src/App.tsx
@@ -2,11 +2,13 @@ import { BrowserRouter, Routes } from 'react-router-dom';
 import getAppRoutes from './appRoutes';
 import Navbar from './components/Navbar';
 import Footer from './components/Footer';
+import { Toaster } from 'sonner';
 
 export default function App() {
   return (
     <div className="overflow-x-auto">
       <BrowserRouter>
+        <Toaster richColors/>
       <Navbar />
         <Routes>
           {getAppRoutes()}
--- src/features/Login/pages/LoginPage.tsx
+++ src/features/Login/pages/LoginPage.tsx
@@ -1,31 +1,26 @@
-import { useState } from 'react';
 import LoginForm from '../components/LoginForm';
 import { useNavigate } from 'react-router-dom';
+import { toast } from 'sonner';
 
 export default function LoginPage() {
   const navigate = useNavigate();
-  const [error, setError] = useState('');
+
 
   const handleLogin = (email: string, password: string) => {
     const fake_email = "hola@gmail.com";
     const fake_password = "password123";
 
     if (email === fake_email && password === fake_password) {
-      setError('');
       navigate('/home');
+      toast['success']('Bienvenido de nuevo!');
     } else {
-      setError('Datos incorrectos, por favor inténtelo otra vez');
+      toast['error']('Error de autenticación. Verifique sus credenciales.');
     }
   };
 
   return (
     <div className="min-h-screen flex items-center justify-center bg-gray-100">
       <div>
-        {error && (
-          <div className="mb-4 px-4 py-3 rounded bg-red-100 text-red-700 text-center font-semibold border border-red-300">
-            {error}
-          </div>
-        )}
         <LoginForm onSubmit={handleLogin} />
       </div>
     </div>

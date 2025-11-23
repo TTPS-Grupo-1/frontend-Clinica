a3b759a6edf9456bd492c307736ce87836893c9e
Facundo Feliu <137841541+faacu02@users.noreply.github.com>
2025-11-23 00:25:30 -0300

    Feature/pagar deuda paciente (#11)
    * fix
    
    * agrego page para consultar deudas y pagar por un paciente
    
    * se muestran las deudas y cobros de pacientes
    
    * Tabla de obras sociales pagos y deudas
    
    * se cobra a la obra social seleccionada

--- src/appRoutes.tsx
+++ src/appRoutes.tsx
@@ -34,6 +34,9 @@ import TratamientoPage from './features/Tratamiento/pages/TratamientoPage';
 import PrimeraConsultaViewPage from './features/Tratamiento/pages/PrimeraConsultaViewPage';
 import SegundaConsultaViewPage from './features/Tratamiento/pages/SegundaConsultaViewPage';
 import DeudaPaciente from './features/Paciente/pages/DeudaPaciente';
+import DeudaPacientesPage from './features/Admin/pages/DeudaPacientesPage';
+import ObrasSocialesFinanzasPage from './features/Admin/pages/ObrasSocialesFinanzasPage';
+
 
 export default function getAppRoutes() {
   return (
@@ -75,6 +78,10 @@ export default function getAppRoutes() {
       <Route path="medico/seguimiento/:pacienteId" element={<RegistrarSeguimientoPage />} />
       <Route path="/tratamiento/:id" element={<TratamientoPage />} />
       <Route path="/pacientes/deuda" element={<DeudaPaciente />} />
+      <Route path="/pacientes/deudaPacientes" element={<DeudaPacientesPage />} />
+      <Route path="/obras-sociales/deuda" element={<ObrasSocialesFinanzasPage />} />
+
+   
 
     </>
   );
